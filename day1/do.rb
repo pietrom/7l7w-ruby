7b8cb1a7791dd34a67d1d04f29e3ef13ba594a25
Pietro Martinelli <amicofragile@gmail.com>
2013-03-16 22:24:21 +0100

    Printed ten times my name

--- day1/do.rb
+++ day1/do.rb
@@ -4,3 +4,9 @@ a_string = 'Hello, Ruby!'
 to_search = 'Ruby'
 index = a_string.index(to_search)
 puts "Index of '#{to_search}' in '#{a_string}' is: #{index}"
+
+my_name = 'Pietro'
+count = 10
+count.times {
+	puts my_name
+}
