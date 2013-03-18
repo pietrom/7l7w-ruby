68c042d4d47e06e192ca706268ed492d4bf6b2ab
Pietro Martinelli <amicofragile@gmail.com>
2013-03-18 22:19:35 +0100

    Reading about hashes

--- day2/reading.rb
+++ day2/reading.rb
@@ -10,3 +10,11 @@ popped = fruits.pop
 puts "popped: #{popped}, size: #{fruits.size}"
 popped = fruits.pop
 puts "popped: #{popped}, size: #{fruits.size}"
+
+tours = {'indurain' => 5, 'merckx' => 5, 'armstrong' => 0}
+puts tours
+puts tours['indurain']
+
+tours.each_key { |key|
+	puts "#{key} won #{tours[key]} tours"
+}
