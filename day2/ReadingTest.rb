ab5db3f53aea52c15543888069a47b9c14cc8d4d
Pietro Martinelli <amicofragile@gmail.com>
2013-03-20 15:41:56 +0000

    TEsting hashes

--- day2/ReadingTest.rb
+++ day2/ReadingTest.rb
@@ -5,7 +5,7 @@ class ReadingTest < Test::Unit::TestCase
     fruits = ['lemon', 'orange', 'peach', 'pineapple']
     assert_equal(4, fruits.size)
   end
-  
+
   def test_array_push_and_pop
     fruits = ['lemon', 'orange', 'peach', 'pineapple']
     fruits.push('strawberry')
@@ -15,5 +15,21 @@ class ReadingTest < Test::Unit::TestCase
     assert_equal(4, fruits.size)
     assert_equal('strawberry', popped)
     assert_equal('pineapple', fruits[3])
+  end
+
+  def test_hashes
+    tours = {'indurain' => 5, 'merckx' => 5, 'armstrong' => 0}
+    assert_equal(0, tours['armstrong'])
+    assert_equal(5, tours['indurain'])
+    assert_nil(tours['simoni'])
+  end
+  
+  def test_hashes_each_pair
+    tours = {'indurain' => 5, 'merckx' => 5, 'armstrong' => 0}
+    buffer = ''
+    tours.each_pair do |key, value|
+      buffer = buffer + key + ':' + value + ';'
+    end
+    assert_equal
   end
 end
